1300678cb05a6c9930a3f6a6213247604ca6c2de
Carlos Augusto Quintero <carlos.qv@alliedglobal.com>
2025-10-15 15:17:57 -0500

    fix: new welcome v2

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo desde calibraite!");
+        System.out.println("Hola Mundo desde calibraite! v2 ");
     }
 }
