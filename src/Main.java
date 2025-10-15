dd5b9e55ae04723550fead238ab9cdbe2dea181a
JorgeMarroquín <Jorge.MO@alliedglobal.com>
2025-10-15 14:14:37 -0600

    fix: add new line

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo desde calibraite!");
+        System.out.println("Gran dia para usar CalibrAite.");
     }
 }
